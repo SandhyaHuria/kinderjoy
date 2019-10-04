b1fcf913d5dfdb61e251bf6ed708739fe550ce7b
rahuldua <rahuldua@192.168.2.13>
2019-10-04 04:53:29 -0400

    First Commit
    Uploading all files

---
+++ airticket/src/test/java/com/cheapfly/airticket/AirticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheapfly.airticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
